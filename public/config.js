43db459393a8309dbe72e53fe2792db386d9e25f
18504142787 <2737057067@qq.com>
2024-08-03 23:29:44 +0800

    改test

--- public/config.js
+++ public/config.js
@@ -1,13 +1,13 @@
 /*全局配置文件，url类型的必须以"/"结尾*/
 var vueEnv = {
   //当前激活的环境
-  active: "prod",
+  active: "test",
   //开发环境
   dev: {
     serverUrl: "http://127.0.0.1:8060/api/",
   },
   test: {
-    serverUrl: "http://47.122.17.43:8060/api/",
+    serverUrl: "http://39.100.143.137:8060/api/",
   },
   prod: {
     serverUrl: "http://47.92.30.191:8060/api/",
